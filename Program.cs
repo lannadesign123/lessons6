d799d11d7e27bfd94884ff027f6b97ec96a3a5f8
svetlana <lannadesign123@gmail.com>
2022-12-11 19:23:11 +0300

    lessons6

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/*
+void Number(int[] M)
+{
+    int sum = 0;
+
+    for (int i = 0; i < M.Length; i++)
+    {
+        if (M[1] > 0)
+    
+        sum++;
+
+        Console.WriteLine($"количество значений больше 0 = {sum}");
+    }            
+}
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] array = Number(num);
+*/
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем. b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+/*
+Console.Write("введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;     
+
+Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+*/
